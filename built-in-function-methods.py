c6e984e1000be0931146e2dc693c7ab6c2c22540
CodeBlueOcean <95150255+CodeBlueOcean@users.noreply.github.com>
2022-02-18 22:39:26 -0600

    Add files via upload

---
+++ built-in-function-methods.py
@@ -0,0 +1,33 @@
+## examples we did in the past
+# str()
+# int()
+# float()
+# print()
+
+#len
+print(len('hellloooo'))
+
+# another example
+# greet = 'hellloooo'
+# print(greet[0:len(greet)])
+# .format()
+
+##.format() is a string is a method
+
+#quote = 'to be or not to be'
+
+quote = 'to be or not to be'
+
+quote2 = quote.replace('be', 'me')
+
+# print(quote.replace('be', 'me'))
+
+# print(quote.find('be'))
+
+# print(quote.upper()) 
+
+# print(quote.capitalize())
+
+print(quote)
+
+print(quote2)
